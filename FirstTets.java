37c88e2b3dcfb7c281d13e29064b54aa30ee5368
= <u23271982@utp.edu.pe>
2024-10-18 22:48:15 -0500

    Commit Secundario

---
+++ FirstTets.java
@@ -0,0 +1,6 @@
+public class FirstTets {
+    public static void main(String[] args) {
+        System.out.println("Hello my firts test in Git and GitHub");
+        System.out.println("Hola mi primera prueba en Git y GitHub");
+    }
+}
